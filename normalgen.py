0c5c58fe5c524a577d3cac504691f0b8b6e3edb3
Talha Ahmed <talha.ahmed@gmail.com>
2018-11-08 16:07:47 +0500

    normal gen with opencv

--- normalgen.py
+++ normalgen.py
@@ -1,40 +1,69 @@
-from PIL import Image
 import numpy as np
 import math
 import os
 
-
-image_path = 'c:/Users/Quixel/Downloads/stones-right.png'
-
-image_dir, image_name = os.path.split(image_path)
-image_name, image_ext = os.path.splitext(image_name)
+from scipy.ndimage import filters
+import cv2
 
 
-im = Image.open(image_path)
-gray = im.convert('L')
-
-new_img = Image.new('RGB', im.size, (0,255,0))
+import operators
 
 
-print ( im.size )
-for x in range(im.size[0]):
-    for y in range(im.size[1]):
-        if x < im.size[0]-1 and x > 0 and y < im.size[1]-1 and y > 0:
-            dx = gray.getpixel((x+1, y)) - gray.getpixel((x-1, y))
-            dy = gray.getpixel((x, y+1)) - gray.getpixel((x, y-1))
-            k = np.array((
-                (dx+255)/2,
-                (dy+255)/2,
-                255))
-            k = k / np.linalg.norm(k)
-            k *= 255
-            new_img.putpixel(
-                    (x, y), (int(k[0]), int(k[1]), int(k[2])))
-
-gray.show()
-new_img.show()
+def makeGray(image):
+    print ( image.shape )
+    if len(image.shape) == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
 
 
-new_img.save(os.path.join(image_dir, image_name + '_NMAP.' + image_ext))
+def generateNormals(hmap, method='sobel', stength=2.5, level=7):
+    ''' Generates Normals given a normal map'''
+    hmap = cv2.cvtColor(hmap, cv2.COLOR_BGR2GRAY)
+    hmap = hmap / 255 # should we linearize
+    op = operators.diffops.get(method, operators.sobel)
+
+    dy = filters.convolve(hmap, op, mode='wrap')
+    dx = filters.convolve(hmap, op.transpose(), mode='wrap')
+    dz = np.ones(dy.shape, dy.dtype)
+
+    if math.fabs(strength < 0.01):
+        stregnth = math.copysign(0.1, strength)
+    dz *= 1.0 / strength * (1.0 + math.pow(2.0, level));
+
+    normalmap = np.zeros((*hmap.shape, 3), dx.dtype)
+    normalmap[:,:,0] = dz
+    normalmap[:,:,1] = dy
+    normalmap[:,:,2] = dx
+
+    norm = np.linalg.norm(normalmap, axis=2)
+    normalmap[:,:,0] /= norm
+    normalmap[:,:,1] /= norm
+    normalmap[:,:,2] /= norm
+
+    normalmap = (normalmap + 1) / 2
+    return normalmap.astype('uint8')
 
 
+def main(image_path=r'c:\Users\Quixel\Downloads\dice.jpg', show=True):
+    image_dir, image_name = os.path.split(image_path)
+    image_name, image_ext = os.path.splitext(image_name)
+
+    im = cv2.imread(image_path)
+    gray = makeGray(im)
+
+    normal = generateNormals(gray)
+
+    cv2.imwrite(
+            os.path.join(image_dir, image_name + '_NMAP.' + image_ext),
+            normal)
+
+    if show:
+        cv2.imshow('Original Image', im)
+        cv2.imshow('GrayScale Image', gray)
+        cv2.imshow('Normal Map', normal)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
---
+++ operators.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+central_diff = np.array(
+        [[-1],
+         [ 0],
+         [ 1]])
+
+sobel = np.array(
+        [[-2, -1, -2],
+         [ 0,  0,  0],
+         [ 2,  1,  2]])
+
+scharr = np.array(
+        [[-3, -10, -3],
+         [ 0,   0,  0],
+         [ 3,  10,  3]])
+
+prewitt = np.array(
+        [[-1, -1, -1],
+         [ 0,  0,  0],
+         [ 1,  1,  1]])
+
+diffops = {
+        'Sobel'         : sobel,
+        'Central Diff'  : central_diff,
+        'Shcarr'        : scharr,
+        'Prewitt'       : prewitt
+}
+
